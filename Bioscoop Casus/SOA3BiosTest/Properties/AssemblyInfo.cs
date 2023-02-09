00ae52d31c50126a04c82bad8f1c8123d1798098
Luc Joosten <120277042+lhajoosten@users.noreply.github.com>
2023-02-09 19:31:52 +0100

    New Branch with Test Project

---
+++ Bioscoop Casus/SOA3BiosTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SOA3BiosTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SOA3BiosTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0823eb8a-e4d1-4c40-a378-07bc12579424")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bioscoop Casus/SOA3BiosTest/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+
+namespace SOA3BiosTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void SecondTicketFreeStudentWeekend()
+        {
+
+            var movieScreening10euro = new MovieScreening(new Movie("Sharknado 2"), new DateTime(2023, 2, 5, 8, 30, 0), 10);
+
+            var movieTickets = new List<MovieTicket>() {
+                new MovieTicket(movieScreening10euro, false, 1, 1),
+                new MovieTicket(movieScreening10euro, false, 1, 2)
+            };
+
+            var studentOrder = new Order(1, true);
+            studentOrder.movieTickets.AddRange(movieTickets);
+
+            Assert.Equal(10.0, studentOrder.CalculatePrice());
+        }
+
+        [Fact]
+        public void SecondTicketFreeStudentWeek()
+        {
+
+            var movieScreening10euro = new MovieScreening(new Movie("Sharknado 2"), new DateTime(2023, 2, 6, 8, 30, 0), 10);
+
+            var movieTickets = new List<MovieTicket>() {
+                new MovieTicket(movieScreening10euro, false, 1, 1),
+                new MovieTicket(movieScreening10euro, false, 1, 2)
+            };
+
+            var studentOrder = new Order(1, true);
+            studentOrder.movieTickets.AddRange(movieTickets);
+
+            Assert.Equal(10.0, studentOrder.CalculatePrice());
+        }
+
+        [Fact]
+        public void SecondTicketFreeNonStudent()
+        {
+
+            var movieScreening10euro = new MovieScreening(new Movie("Sharknado 2"), new DateTime(2023, 2, 6, 8, 30, 0), 10);
+
+            var movieTickets = new List<MovieTicket>() {
+                new MovieTicket(movieScreening10euro, false, 1, 1),
+                new MovieTicket(movieScreening10euro, false, 1, 2)
+            };
+
+            var studentOrder = new Order(1, false);
+            studentOrder.movieTickets.AddRange(movieTickets);
+
+            Assert.Equal(10.0, studentOrder.CalculatePrice());
+        }
+
+        [Fact]
+        public void SecondTicketNotFreeNonStudent()
+        {
+
+            var movieScreening10euro = new MovieScreening(new Movie("Sharknado 2"), new DateTime(2023, 2, 5, 8, 30, 0), 10);
+
+            var movieTickets = new List<MovieTicket>() {
+                new MovieTicket(movieScreening10euro, false, 1, 1),
+                new MovieTicket(movieScreening10euro, false, 1, 2)
+            };
+
+            var studentOrder = new Order(1, false);
+            studentOrder.movieTickets.AddRange(movieTickets);
+
+            Assert.Equal(20.0, studentOrder.CalculatePrice());
+        }
+    }
+}
